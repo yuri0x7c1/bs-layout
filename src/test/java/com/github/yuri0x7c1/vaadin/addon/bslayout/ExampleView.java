6c1ee4e30dd43352622872fda1d34006ba2a306b
yuri0x7c1 <yuri0x7c1@gmail.com>
2022-10-08 15:55:53 +0300

    Revert example view

--- src/test/java/com/github/yuri0x7c1/vaadin/addon/bslayout/ExampleView.java
+++ src/test/java/com/github/yuri0x7c1/vaadin/addon/bslayout/ExampleView.java
@@ -29,20 +29,6 @@ import com.vaadin.flow.router.Route;
 public class ExampleView extends VerticalLayout {
 
     public ExampleView() {
-    	/*
-    	// basic usage
-    	BsLayout layout = new BsLayout();
-    	BsRow row = layout.addRow();
-    	BsColumn col1 = row.addColumn(new BsColumn(new Label("Column 1-1")));
-    	col1.addSize(Size.MD, 3);
-    	BsColumn col2 = row.addColumn(new BsColumn(new Label("Column 1-2")));
-    	col2.addSize(Size.MD, 3);
-    	BsColumn col3 = row.addColumn(new BsColumn(new Label("Column 1-3")));
-    	col3.addSize(Size.MD, 3);
-    	col3.addOffset(Offset.MD, 3);
-    	*/
-
-    	/*
     	BsLayout layout = new BsLayout().withRows(
 			new BsRow().withColumns(
 				new BsColumn(new Label("Column 1-1")).withSize(Size.MD, 6),
@@ -53,16 +39,6 @@ public class ExampleView extends VerticalLayout {
 				new BsColumn(new Label("Column 2-1"))
 					.withSize(Size.MD, 6)
 					.withOffset(Offset.MD, 6)
-			)
-		);
-		*/
-
-    	// default column sizes
-    	BsLayout layout = new BsLayout().withDefaultSizes(12, 4, 4, 3, 2).withRow(
-			new BsRow().withColumns(
-				new BsColumn(new Label("Column 1-1")),
-				new BsColumn(new Label("Column 1-2")),
-				new BsColumn(new Label("Column 1-3"))
 			)
 		);
 
