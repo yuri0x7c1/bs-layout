e45b70e1401f322142d435974488aa251a7d26a2
yuri0x7c1 <yuri0x7c1@gmail.com>
2022-11-19 14:59:46 +0200

    Update license header

--- src/test/java/org/vaadin/addons/yuri0x7c1/bslayout/BsLayoutTest.java
+++ src/test/java/org/vaadin/addons/yuri0x7c1/bslayout/BsLayoutTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2022 The original authors
+ * Copyright 2021-2022 yuri0x7c1
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
